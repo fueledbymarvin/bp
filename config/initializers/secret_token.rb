a09f9925ac9cad61ff02fefcc9d9e69d357f6886
Marvin Qian <fueledbymarvin@gmail.com>
2013-11-05 12:34:54 -0500

    Add necessary gems.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bp::Application.config.secret_key_base = 'c421c0ce196ee225e8df6a9b4dd171634d910d76b37ce4e7256447d07df4e95be507e239fa5c5d61fcd1a184736abf2b9efd500875938abaa7a4f7ca8919a24e'
