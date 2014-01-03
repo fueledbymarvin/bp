36982df9672f78719494bfde802cb43204b8836c
Marvin Qian <fueledbymarvin@gmail.com>
2014-01-03 13:26:23 -0500

    Update for strong parameters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,14 +10,25 @@ class PostsController < ApplicationController
   end
 
   def create
-    respond_with Post.create(params[:post])
+    respond_with Post.create(post_params)
   end
 
   def update
-    respond_with Post.update(params[:id], params[:post])
+    respond_with Post.update(params[:id], post_params)
   end
 
   def destroy
     respond_with Post.destroy(params[:id])
   end
+
+private
+
+  def post_params
+    # if current_user && current_user.admin?
+    params.require(:post).permit!
+    # else
+    #   params.require(:post).permit(:title)
+    # end
+  end
+
 end
