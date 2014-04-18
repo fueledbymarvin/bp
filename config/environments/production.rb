dbb3b8f4a3a1e0c237dcf35a94c365b8e3f89b97
Marvin Qian <fueledbymarvin@gmail.com>
2014-04-18 13:38:28 +0000

    Fix js mangling for angular.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,7 @@ Bp::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(mangle: false)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
